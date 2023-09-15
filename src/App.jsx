278dbdabad1128860d2b7d98bd2af28e30172433
AP-Porag <smauhp@gmail.com>
2023-09-15 12:51:26 +0600

    horizone ui commit reverted

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-// import LoginPage from "./pages/Login"
-// import Sidebar from "./components/shop/Sidebar"
+import LoginPage from "./pages/Login"
 
 // function App() {
 //   const [count, setCount] = useState(0)
@@ -36,28 +35,8 @@
 
 // export default App
 
-// export default function App() {
-//   return (
-//     // <LoginPage/>
-//     <Sidebar/>
-//   )
-// }
-
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-
-import RTL from "./layouts/rtl";
-import Admin from "./layouts/admin";
-import Auth from "./layouts/auth";
-const App = () => {
+export default function App() {
   return (
-    <Routes>
-      <Route path="auth/*" element={<Auth />} />
-      <Route path="admin/*" element={<Admin />} />
-      <Route path="rtl/*" element={<RTL />} />
-      <Route path="/" element={<Navigate to="/admin" replace />} />
-    </Routes>
-  );
-};
-
-export default App;
+    <LoginPage/>
+  )
+}
